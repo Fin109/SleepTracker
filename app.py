a96e10bc25ba5fbd29376954ba5233f17f2dc4a7
fin109 <fcarter@unca.edu>
2021-02-09 18:35:21 -0500

    Add labeling to axes', clean up app.py, remove weird app.config['user'] lines

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
-import os, pymongo, json, os
+import os, pymongo
 
 
-# Instantiate Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['PLOTTING']=False
+app.config['plotting']=False
 
-# Start login manager
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Start MongoDB Atlas
+#Database
 client = pymongo.MongoClient(os.getenv('DATABASE'))
 db = client.user_login
 
-# Define user class
+
+## Define flask-login User class ## 
 class User(UserMixin):
     def __init__(self, username):
         self.username = username
+
     def get_id(self):
         return self.username
 
@@ -31,46 +32,64 @@ class User(UserMixin):
 
     @login_manager.user_loader
     def load_user(username):
-        user = db.users.find_one({'username': username})
+        user = db.users.find_one({"username": username})
         if user is None:
             return None
         return User(username=user['username'])
 
-@app.route('/')
-def login():
-	return render_template('login.html')
 
-@app.route('/', methods = ['POST'])
+## Login/Register page
+@app.route("/")
+def login():
+	return render_template("login.html")
+
+
+@app.route("/", methods = ["POST"])
 def login_or_register():
     if request.method == 'POST':
-        name_entered = request.form.get('user_name')
+        name_entered = request.form.get('user_name') # Get username and password from form
         pw_entered = request.form.get('user_pw')
 
         if request.form.get('login'): # Log in logic
             user = db.users.find_one({'username':name_entered})
-
             if user and User.check_password(user['password'],pw_entered):
                 usr_obj = User(username=user['username'])
                 login_user(usr_obj)
-                app.config['user'] = user
                 return redirect(url_for('main'))
             else:
-                return redirect(url_for('index'))
+                flash("Incorrect username and password combination") 
+                return redirect(url_for('login'))
 
         elif request.form.get('register'): # Register logic
             new_user = {'username':name_entered,'password':pw_entered}
             db.users.insert_one(new_user) # insert new user to db
-            return redirect(url_for('index'))
+            return redirect(url_for('login')) # redirect after register
 
-@app.route('/main')
+
+# Page where user logs sleep
+@app.route("/main")
 def main():
-    # if app.config['plotting'] == True, user has clicked "view graph"
-    return render_template('main.html',user=app.config['user'],plot=app.config['PLOTTING'])
+    user_data = db.users.find_one({'username':current_user.get_id()})
+    return render_template("main.html",user=user_data,plot=app.config['plotting'])
 
 
-# When functionality for submit, logout, plot buttons
-@app.route('/main', methods=['POST'])
-def submit_logout_plot():
+# Function for adding sleep data
+def add_sleep(time,date,user):
+    if 'date' in user:
+        user['date'].append(date)
+        user['time'].append(time)
+    else: # If first time adding sleep data
+        user['date'] = [date]
+        user['time'] = [time]
+
+    # Update MongoDB Atlas
+    db.users.update_one({'username':user['username']},
+        {'$set':{'date':user['date'],
+        'time':user['time']}})
+
+
+@app.route('/main', methods=['POST','GET'])
+def submit_sleep():
     if request.method == 'POST':
 
         if request.form.get('submit'): # if submitting new sleep data
@@ -80,26 +99,14 @@ def submit_logout_plot():
 
         if request.form.get('logout'):
             logout_user()
-            app.config['PLOTTING'] = False
+            app.config['plotting'] = False
             return 'You logged out!'
-        else:
-            app.config['PLOTTING'] = True
+
+        else: # If user has clicked "view graph", set plotting = True, redirect to '/main'
+            app.config['plotting'] = True
 
     return redirect(url_for('main'))
-
-
-# add sleep data to MongoDB Atlas
-def add_sleep(time,date,user):
-    if 'date' in user:
-        user['date'].append(date)
-        user['time'].append(time)
-    else:
-        user['date'] = [date]
-        user['time'] = [time]
-
-    db.users.update_one({'username':user['username']},
-        {'$set':{'date':user['date'],
-        'time':user['time']}})
     
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	app.run(debug=True)
